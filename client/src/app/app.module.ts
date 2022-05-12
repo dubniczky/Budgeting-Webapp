293c308f057ec17316d4f9e6fc03541507a989f5
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-12 16:56:39 +0200

    Fixed line endings

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetListComponent
   ],
   imports: [
     BrowserModule,
