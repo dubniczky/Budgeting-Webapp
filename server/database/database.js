7272983a5122e381115c62c7d18620cdfbe998ee
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-26 19:32:04 +0200

    Added database module

--- server/database/database.js
+++ server/database/database.js
@@ -1,20 +1,64 @@
 import fs from 'fs'
+import path from 'path'
 import sqlite from 'better-sqlite3'
 
+import { BudgetItem } from 'budget/model/budget-item.js'
+
 const dbpath = 'db.sqlite'
-const migrate = false
+let clean_db = false
 
 if (!fs.existsSync(dbpath)) {
-    migrate = true
+    clean_db = true
 }
 
 const db = new sqlite(dbpath)
 
-export function query(sql, ... params) {
+if (clean_db) {
+    migrate()
+    seed()
+}
+
+export function query(sql, params) {
     db.prepare(sql).all(params)
 }
 
+export function exec(sql, params) {
+    db.prepare(sql).run(params)
+}
+
 export function migrate() {
-    const sql = fs.readFileSync('database/budget.create.sql')
+    const file = path.resolve('database/budget.create.sql')
+    const sql = fs.readFileSync( file, 'utf8' )
     db.exec(sql)
+}
+
+export function seed() {
+    let budgets = [
+        { id: 0, title: 'Groceries', value: -315.0, category: 'food' },
+        { id: 1, title: 'Eating out', value: -50.0, category: 'food' },
+        { id: 2, title: 'Rent', value: -900.0, category: 'housing' },
+        { id: 3, title: 'Bills', value: -112.0, category: 'housing' },
+        { id: 4, title: 'Car', value: -45.0, category: 'trasport' },
+        { id: 5, title: 'Public Transport', value: -35.0, category: 'trasport' },
+        { id: 6, title: 'Salary', value: 5500, category: 'job' },
+        { id: 7, title: 'Benefits', value: 150, category: 'job' },
+    ]
+
+    for (let b in budgets) {
+        exec('INSERT INTO budgets (title, c_value, category) VALUES (?, ?, ?)',
+            [b.title, b.value, b.category])
+    }
+}
+
+export function toBudgets(qres) {
+    let budgets = []
+    for (let b in budgets) {
+        budgets.push( new BudgetItem(b.id, b.title, b.c_value, b.category) )
+    }
+    return budgets
+}
+
+export default {
+    db,
+    query,
 }
--- server/routers/budget.router.js
+++ server/routers/budget.router.js
@@ -1,17 +1,7 @@
 import express from 'express'
+import db from 'budget/database/database.js'
 
 const router = express.Router()
-
-let list = [
-    { id: 0, title: 'Groceries', value: -315.0, category: 'food' },
-    { id: 1, title: 'Eating out', value: -50.0, category: 'food' },
-    { id: 2, title: 'Rent', value: -900.0, category: 'housing' },
-    { id: 3, title: 'Bills', value: -112.0, category: 'housing' },
-    { id: 4, title: 'Car', value: -45.0, category: 'trasport' },
-    { id: 5, title: 'Public Transport', value: -35.0, category: 'trasport' },
-    { id: 6, title: 'Salary', value: 5500, category: 'job' },
-    { id: 7, title: 'Benefits', value: 150, category: 'job' },
-]
 
 router.get('/', (req, res) => {
     res.json(list)
