a01712c8c1eb8e56f5d64d24577354f0231faec7
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-26 17:02:36 +0200

    Updated serverrouting model

--- server/main.js
+++ server/main.js
@@ -1,23 +1,22 @@
 import express from 'express'
 
 import { allowCors } from 'budget/middleware/cors.js'
+import budgetRouter from 'budget/routers/budget.router.js'
+
 
 const PORT = 8080
 
 const app = express()
+
+
+// Install middleware
 app.use(express.json())
 app.use(allowCors)
 
-let list = [
-    { id: 0, title: 'Groceries', value: -315.0, category: 'food' },
-    { id: 1, title: 'Eating out', value: -50.0, category: 'food' },
-    { id: 2, title: 'Rent', value: -900.0, category: 'housing' },
-    { id: 3, title: 'Bills', value: -112.0, category: 'housing' },
-    { id: 4, title: 'Car', value: -45.0, category: 'trasport' },
-    { id: 5, title: 'Public Transport', value: -35.0, category: 'trasport' },
-    { id: 6, title: 'Salary', value: 5500, category: 'job' },
-    { id: 7, title: 'Benefits', value: 150, category: 'job' },
-]
+
+// Install routers
+app.use('/api/budget', budgetRouter)
+
 
 // Respond with CORS headers when the browser is doing preliminary checks
 app.options('*', (req, res) => {
@@ -27,55 +26,14 @@ app.head('*', (req, res) => {
     res.sendStatus(200)
 })
 
-app.get('/api/budget/all', (req, res) => {
-    res.json(list)
+
+// Echo headers (debug)
+app.head('/headers', (req, res) => {
+    res.send( req.headers )
 })
 
-app.get('/api/stats', (req, res) => {
-    let spend = 0
-    let income = 0
 
-    for (let i of list) {
-        let val = i.value
-        if (val > 0) {
-            income += val
-        }
-        else {
-            spend += -val
-        }
-    }
-
-    res.json({
-        income: income,
-        spending: spend
-    })
-})
-
-app.get('/api/budget/get/:id', (req, res) => {
-    let id = req.params['id']
-    res.json(list[id])
-})
-
-app.patch('/api/budget', (req, res) => {
-    let id = req.body.id
-    if (id == null) {
-        return res.sendStatus(400)
-    }
-
-    list[id] = req.body // TODO
-
-    res.json({ ok:1 })
-})
-
-app.post('/api/budget/new', (req, res) => {
-    let item = req.body
-    console.log(item)
-    item.id = list.length
-    list.push(item)
-
-    res.send({ok:1, id:item.id})
-})
-
+// Starting server
 app.listen(PORT, () => {
 	console.log('Listening on:', PORT)
 })
---
+++ server/routers/budget.router.js
@@ -0,0 +1,65 @@
+import express from 'express'
+
+const router = express.Router()
+
+let list = [
+    { id: 0, title: 'Groceries', value: -315.0, category: 'food' },
+    { id: 1, title: 'Eating out', value: -50.0, category: 'food' },
+    { id: 2, title: 'Rent', value: -900.0, category: 'housing' },
+    { id: 3, title: 'Bills', value: -112.0, category: 'housing' },
+    { id: 4, title: 'Car', value: -45.0, category: 'trasport' },
+    { id: 5, title: 'Public Transport', value: -35.0, category: 'trasport' },
+    { id: 6, title: 'Salary', value: 5500, category: 'job' },
+    { id: 7, title: 'Benefits', value: 150, category: 'job' },
+]
+
+router.get('/', (req, res) => {
+    res.json(list)
+})
+
+router.patch('/', (req, res) => {
+    let id = req.body.id
+    if (id == null) {
+        return res.sendStatus(400)
+    }
+
+    list[id] = req.body // TODO
+
+    res.json({ ok:1 })
+})
+
+router.post('/', (req, res) => {
+    let item = req.body
+    console.log(item)
+    item.id = list.length
+    list.push(item)
+
+    res.send({ok:1, id:item.id})
+})
+
+router.get('/:id', (req, res) => {
+    let id = req.params['id']
+    res.json(list[id])
+})
+
+router.get('/stats', (req, res) => {
+    let spend = 0
+    let income = 0
+
+    for (let i of list) {
+        let val = i.value
+        if (val > 0) {
+            income += val
+        }
+        else {
+            spend += -val
+        }
+    }
+
+    res.json({
+        income: income,
+        spending: spend
+    })
+})
+
+export default router
