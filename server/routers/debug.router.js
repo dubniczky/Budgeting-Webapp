980f3353cca087f96c951a68b1809e808fb6fbba
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-27 11:34:51 +0200

    Added echo debug endpoint

--- server/routers/debug.router.js
+++ server/routers/debug.router.js
@@ -2,8 +2,12 @@ import express from 'express'
 
 const router = express.Router()
 
-router.head('/headers', (req, res) => {
+router.get('/headers', (req, res) => {
     res.send( req.headers )
+})
+
+router.post('/echo', (req, res) => {
+    res.send( req.body )
 })
 
 export default router
